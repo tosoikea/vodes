22c98147e8b3d63657e41ef1c976a2dd71d22f36
Torben Soennecken <soennecken@rootitup.de>
2021-04-14 12:29:32 +0200

    [f] Added missing pyyaml reference

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'matplotlib>=3.3.1', 'numpy>=1.20.0' ]
+requirements = ['Click>=7.0', 'matplotlib>=3.3.1', 'numpy>=1.20.0', 'pyyaml>=5.4' ]
 
 setup_requirements = ['pytest-runner', ]
 
