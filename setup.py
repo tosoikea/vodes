ece5f0cc686816f46f5cbfbd9fdf0c46c1f4c4e4
Torben Soennecken <soennecken@rootitup.de>
2021-04-14 12:26:47 +0200

    [~] Allow earlier libraries

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'matplotlib>=3.4.1', 'numpy>=1.20.2' ]
+requirements = ['Click>=7.0', 'matplotlib>=3.3.1', 'numpy>=1.20.0' ]
 
 setup_requirements = ['pytest-runner', ]
 
